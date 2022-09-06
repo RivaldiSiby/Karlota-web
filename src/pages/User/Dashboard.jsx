dae22e214de0ad154b297cec4e5ce82efc39b610
Rivaldi Siby <rivaldisiby@gmail.com>
2022-09-07 01:15:06 +0800

    add user info

--- src/pages/User/Dashboard.jsx
+++ src/pages/User/Dashboard.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import NavbarUser from "../../components/layout/NavbarUser";
+
+// img
+import userImg from "../../assets/img/user.jpg";
 
 function Dashboard() {
   const user = useSelector((state) => state.auth);
@@ -10,9 +14,24 @@ function Dashboard() {
       <NavbarUser />
       <main className="wrap-dash">
         <section className="main-body-dash container">
-          <section className="main-user-info">user</section>
-          <section className="main-box-wrap ">contact</section>
-          <section className="main-contact-list ">content</section>
+          <div className="main-user-info">
+            <section className="profile-user border d-flex align-items-center justify-content-center">
+              <img src={userImg} className="img-profile-user" alt="user" />
+            </section>
+            <section className="profile-info border d-flex flex-column justify-content-center bg-danger">
+              <h5>Rivaldi Siby</h5>
+              <p>rivaldisiby@gmail.com</p>
+              {/* <h4>Gemini</h4> */}
+            </section>
+            <section className="profile-desc border">Desc</section>
+          </div>
+          <div className="main-box-wrap ">contact</div>
+          <div className="main-contact-list ">
+            <section>Contact</section>
+            <section>Contact</section>
+            <section>Contact</section>
+            <section>Contact</section>
+          </div>
         </section>
       </main>
     </>
